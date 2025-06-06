4a95d8949ade1c1b3ae4e898086e7d0ad9b1028c
JerThePear73 <Jerrynuggets23@gmail.com>
2025-06-06 00:17:11 -0600

    Revert "Davy merging wip"
    This reverts commit 84d97a8914a9c840349974f8fcc68a89ea7ab969.

--- commands.lua
+++ commands.lua
@@ -23,14 +23,6 @@ local function command_moveset_guide(msg)
 		djui_chat_message_create("Wing Cap + Ground Pound Jump = Rocket Jump")
 		djui_chat_message_create("Wing Cap + Dive (Ground) + Z = Slip n' Slide")
 		return true
-	elseif msg == "davy" or (msg == "" and is_davy()) then
-		djui_chat_message_create("\\#ff8000\\Davy (Moves):")
-		djui_chat_message_create("GP (Air) + B = Airborne Slide Kick")
-		djui_chat_message_create("GP (Ground) + A = Skeletal Missile")
-		djui_chat_message_create("Punch = Bomb Toss")
-		djui_chat_message_create("Air + A = Flutter")
-		djui_chat_message_create("Dive (Air) + A = Hover Missile")
-		return true
 	end
 	return false
 end
@@ -52,9 +44,6 @@ local function command_moveset_passives(msg)
 		djui_chat_message_create("- Ice Flower (Metal Cap) allows skating on water and lava")
 		djui_chat_message_create("- Fludd (Wing Cap) will last as long as you have water, no timer!")
 		return true
-	elseif msg == "davy" or (msg == "" and is_davy()) then
-		djui_chat_message_create("- Faster while carrying things")
-		djui_chat_message_create("- Coins grant bombs faster")
 	end
 	return false
 end
@@ -77,14 +66,6 @@ local function command_jess_fludd_coins(msg)
 	return false
 end
 
-function command_reset_save()
-	mod_storage_save_number("davyProgress", 0)
-	djui_chat_message_create("[Davy save reset]")
-	davyProgress = 0
-	return true
-end
-
-hook_chat_command("jj-moves", "[jer|jess|davy] - shows moveset guide for the character", command_moveset_guide)
-hook_chat_command("jj-passives", "[jer|jess|davy] - shows passives for the character", command_moveset_passives)
+hook_chat_command("jj-moves", "[jer|jess] - shows moveset guide for the character", command_moveset_guide)
+hook_chat_command("jj-passives", "[jer|jess] - shows passives for the character", command_moveset_passives)
 hook_chat_command("jj-fludd-coins", "- toggles whether collecting coins should refill Fludd water (host only)", command_jess_fludd_coins)
-hook_chat_command("jj-resetsave", " - resets Davy unlock progress", command_reset_save)
